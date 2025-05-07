222913d4b52c229315234bb9b711ea5e98adc522
Heet <heetnandu01@gmail.com>
2025-05-07 21:40:16 +0530

    basics of java started

---
+++ java-basics/main.java
@@ -0,0 +1,38 @@
+// basic java file 
+
+// public class main {
+//     public static void main(String[] args) {
+//         int a = 25;
+//         System.out.println(a);
+//     }   
+// }
+
+//Primitive
+
+// public class main {
+//     public static void main(String[] args) {
+//         int a = 25;
+//         float pi = 3.14f;
+//         char grade = 'A';
+//         boolean isValid = true;
+//         System.out.println(a);
+//         System.out.println(pi);
+//         System.out.println(grade);
+//         System.out.println(isValid);;
+//     }
+// }
+
+// Non primitive 
+
+// public class main {
+//     public static void main(String[] args) {
+//         String name = "Heet";
+//         System.out.println(name);
+
+//         int numbers[] = {1,2,3,4,5};
+//         System.out.println(numbers);
+//     }
+// }
+
+
+
