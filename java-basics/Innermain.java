bdec74a1ced08b29f5e31559d48b83990955d971
Heet <heetnandu01@gmail.com>
2025-05-08 23:52:59 +0530

    java basics

---
+++ java-basics/Innermain.java
@@ -0,0 +1,74 @@
+// basic java file 
+
+// public class main {
+//     public static void main(String[] args) {
+//         int a = 25;
+//         System.out.println(a);
+//     }   
+// }
+
+//Primitive
+
+// public class main {
+//     public static void main(String[] args) {
+//         int a = 25;
+//         float pi = 3.14f;
+//         char grade = 'A';
+//         boolean isValid = true;
+//         System.out.println(a);
+//         System.out.println(pi);
+//         System.out.println(grade);
+//         System.out.println(isValid);;
+//     }
+// }
+
+// Non primitive 
+
+// public class main {
+//     public static void main(String[] args) {
+//         String name = "Heet";
+//         System.out.println(name);
+
+//         int numbers[] = {1,2,3,4,5};
+//         System.out.println(numbers);
+//     }
+// }
+
+
+// public class main {
+//     public static void main(String[] args) {
+//         int a = 7;
+//         int b = 4;
+//         System.out.println(a > b);
+//         System.out.println(a + b);
+
+
+
+//         if(a >b){
+//             System.out.println("A is Bigger");
+//         }
+//         else {
+//             System.out.println("B is smaller");
+//         }
+//     }
+// }
+
+// import java.util.Scanner;
+
+// public class Innermain {
+//     public static void main(String[] args) {
+//         System.out.print("Enter the week day: ");
+//         Scanner sc = new Scanner(System.in);
+//         int day = sc.nextInt();
+//         switch(day){
+//             case 1:System.out.println("Monday");
+//             break;
+//             case 2:System.out.println("Tuesday");
+//             break;
+//             case 3:System.out.println("Wednesday");
+//             break;
+//             default:System.out.println("Other day");
+//             break;
+//         }
+//     }
+// }
